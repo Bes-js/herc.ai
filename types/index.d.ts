dffee929b08a25a9537cd0201b7def0d967f9d2b
Five So Beş <89789101+Bes-js@users.noreply.github.com>
2023-09-26 03:30:43 +0300

    Update index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,18 @@
 import Hercai from "./src/hercai";
+
+type QuestionData = {
+content:string,
+reply:string,
+};
+
+type DrawImageData = {
+model:string,
+prompt:string,
+url:string
+};
+
 export {
 Hercai,
+QuestionData,
+DrawImageData
 };
