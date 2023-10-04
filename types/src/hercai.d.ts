b31c2de97924ff258c28cd0fade1cc9bd834bdf7
Five So Beş <89789101+Bes-js@users.noreply.github.com>
2023-10-04 08:33:30 +0300

    Update hercai.d.ts

--- types/src/hercai.d.ts
+++ types/src/hercai.d.ts
@@ -12,25 +12,26 @@ declare class Hercai {
 * The Question You Want to Ask Artificial Intelligence.
 * @param {string} model "v2"
 * @param {string} model "beta"
+* @param {string} model "v3-beta" (GPT-4)
 * @param {string} content The Question You Want to Ask Artificial Intelligence.
 * @example client.question({model:"v2",content:"how are you?"})
 * @type {string} The Question You Want to Ask Artificial Intelligence.
 * @returns {Hercai}
 * @async
 */
-question(object:{model:"v2"|"beta",content:string}):Promise<{content:string,reply:string}>;
+question(object:{model:"v2"|"beta"|"v3-beta",content:string}):Promise<{content:string,reply:string}>;
 
 
- /**
+/**
 * Tell Artificial Intelligence What You Want to Draw.
-* @param {string} model "v1" , "v2" , "v2-beta"
+* @param {string} model "v1" , "v2" , "v2-beta" , "v3" (DALL-E) , "lexica" , "prodia"
 * @param {string} prompt Tell Artificial Intelligence What You Want to Draw.
 * @example client.drawImage({model:"v1",prompt:"anime girl"})
 * @type {string} Tell Artificial Intelligence What You Want to Draw.
 * @returns {Hercai}
 * @async
 */
- drawImage(object:{model:"v1"|"v2"|"v2-beta",prompt:string}):Promise<{model:string,prompt:string,url:string}>;
+ drawImage(object:{model:"v1"|"v2"|"v2-beta"|"v3"|"lexica"|"prodia",prompt:string}):Promise<{model:string,prompt:string,url:string}>;
 
 
 }
