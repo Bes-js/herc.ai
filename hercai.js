8fce20f67dc478bb431eceb455d2c5c0ce9d5269
Five So Beş <89789101+Bes-js@users.noreply.github.com>
2023-10-04 08:33:56 +0300

    Update hercai.js

--- hercai.js
+++ hercai.js
@@ -7,6 +7,7 @@ const baseurl = "https://hercai.onrender.com/v2/hercai";
 /**
  * @typedef {Class} Hercai
  * @see {Hercai}
+ * @param {Class} Hercai
  * @example const { Hercai } = require("hercai");
  * @example import { Hercai } from "hercai";
  * @type {Class}
@@ -20,6 +21,7 @@ class Hercai {
 * The Question You Want to Ask Artificial Intelligence.
 * @param {string} model "v2"
 * @param {string} model "beta"
+* @param {string} model "v3-beta" (GPT-4)
 * @param {string} content The Question You Want to Ask Artificial Intelligence.
 * @example client.question({model:"v2",content:"how are you?"})
 * @type {string} The Question You Want to Ask Artificial Intelligence.
@@ -27,7 +29,7 @@ class Hercai {
 * @async
 */
 async question({model = "v2",content}){
-if(!["v2","beta"].some(ind => model == ind)) model = "v2";
+if(!["v2","beta","v3-beta"].some(ind => model == ind)) model = "v2";
 if(!content || content == undefined || content == null)throw new Error("Please specify a question!");
 try{
 var api = await axios.get(`https://hercai.onrender.com/${model}/hercai?question=`+encodeURI(content),{
@@ -43,7 +45,7 @@ throw new Error("Error: "+ err.message)
 
 /**
 * Tell Artificial Intelligence What You Want to Draw.
-* @param {string} model "v1" , "v2" , "v2-beta"
+* @param {string} model "v1" , "v2" , "v2-beta" , "v3" (DALL-E) , "lexica" , "prodia"
 * @param {string} prompt Tell Artificial Intelligence What You Want to Draw.
 * @example client.drawImage({model:"v1",prompt:"anime girl"})
 * @type {string} Tell Artificial Intelligence What You Want to Draw.
@@ -51,7 +53,7 @@ throw new Error("Error: "+ err.message)
 * @async
 */
 async drawImage({model = "v1",prompt}){
-    if(!["v1","v2","v2-beta"].some(ind => model == ind)) model = "v1";
+    if(!["v1","v2","v2-beta","v3","lexica","prodia"].some(ind => model == ind)) model = "prodia";
     if(!prompt || prompt == undefined || prompt == null)throw new Error("Please specify a prompt!");
     try{
     var api = await axios.get(`https://hercai.onrender.com/${model}/text2image`+"?prompt="+encodeURI(prompt),{
