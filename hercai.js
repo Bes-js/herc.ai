5bae5d866c73cea2150a22208fc0e0bdcfc2ea6c
Five So Beş <89789101+Bes-js@users.noreply.github.com>
2024-01-06 06:25:49 +0300

    News Models & Features Added.

--- hercai.js
+++ hercai.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const axios = require('axios');
-const baseurl = "https://hercai.onrender.com/v3-beta/hercai";
+const baseurl = "https://hercai.onrender.com/v3/hercai";
 
 
 /**
@@ -21,7 +21,10 @@ class Hercai {
 
 /**
 * The Question You Want to Ask Artificial Intelligence.
-* @param {string} model "v3-beta" (GPT-4)
+* @param {string} model "v3" (GPT-4)
+* @param {string} model "v3-32k" (GPT-4-32k)
+* @param {string} model "turbo" (GPT-3.5 Turbo)
+* @param {string} model "turbo-16k" (GPT-3.5 Turbo-16k)
 * @param {string} model "gemini" (Google Gemini-Pro)
 * @param {string} content The Question You Want to Ask Artificial Intelligence.
 * @example client.question({model:"v3-beta",content:"how are you?"})
@@ -29,8 +32,8 @@ class Hercai {
 * @returns {Hercai}
 * @async
 */
-async question({model = "v3-beta",content}){
-if(!["v3-beta","gemini"].some(ind => model == ind)) model = "v3-beta";
+async question({model = "v3",content}){
+if(!["v3","gemini","v3-32k","turbo","turbo-16k"].some(ind => model == ind)) model = "v3";
 if(!content || content == undefined || content == null)throw new Error("Please specify a question!");
 try{
 var api = await axios.get(`https://hercai.onrender.com/${model}/hercai?question=`+encodeURI(content),{
@@ -49,18 +52,20 @@ throw new Error("Error: "+ err.message)
 
 /**
 * Tell Artificial Intelligence What You Want to Draw.
-* @param {string} model "v1" , "v2" , "v2-beta" , "v3" (DALL-E) , "lexica" , "prodia"
+* @param {string} model "v1" , "v2" , "v2-beta" , "v3" (DALL-E) , "lexica" , "prodia", "simurg", "animefy", "raava", "shonin"
 * @param {string} prompt Tell Artificial Intelligence What You Want to Draw.
-* @example client.drawImage({model:"v1",prompt:"anime girl"})
+* @param {string} negative_prompt It includes the features that you do not want to be included in the output you want from artificial intelligence.
+* @example client.drawImage({model:"v3",prompt:"anime girl"})
 * @type {string} Tell Artificial Intelligence What You Want to Draw.
 * @returns {Hercai}
 * @async
 */
-async drawImage({model = "v3",prompt}){
-    if(!["v1","v2","v2-beta","v3","lexica","prodia"].some(ind => model == ind)) model = "prodia";
+async drawImage({model = "v3",prompt,negative_prompt}){
+    if(!["v1","v2","v2-beta","v3","lexica","prodia","simurg","animefy","raava","shonin"].some(ind => model == ind)) model = "prodia";
     if(!prompt || prompt == undefined || prompt == null)throw new Error("Please specify a prompt!");
+    if(!negative_prompt || negative_prompt == undefined || negative_prompt == null) negative_prompt = "";
     try{
-    var api = await axios.get(`https://hercai.onrender.com/${model}/text2image`+"?prompt="+encodeURI(prompt),{
+    var api = await axios.get(`https://hercai.onrender.com/${model}/text2image`+"?prompt="+encodeURI(prompt)+"&negative_prompt="+encodeURI(negative_prompt),{
         headers: {
             "content-type": "application/json",
         },
