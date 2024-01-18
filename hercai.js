d747360e09a553d0c9cb5bdddb865dbf3bae952b
Five So Beş <89789101+Bes-js@users.noreply.github.com>
2024-01-19 01:03:26 +0300

    Update hercai.js

--- hercai.js
+++ hercai.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const axios = require('axios');
+const axios = require('axios').default;
 const baseurl = "https://hercai.onrender.com/v3/hercai";
 
 
@@ -14,7 +14,7 @@ const baseurl = "https://hercai.onrender.com/v3/hercai";
  * @class
  */
 class Hercai {
-    constructor(apiKey) {
+    constructor(apiKey="") {
     if(apiKey == undefined || apiKey == null || apiKey == "" || typeof apiKey !== "string")this.apiKey = "";
     this.apiKey = apiKey;
     };
@@ -27,22 +27,24 @@ class Hercai {
 * @param {string} model "turbo-16k" (GPT-3.5 Turbo-16k)
 * @param {string} model "gemini" (Google Gemini-Pro)
 * @param {string} content The Question You Want to Ask Artificial Intelligence.
+* @param {string} personality It includes the features that you want to be included in the output you want from artificial intelligence.
 * @example client.question({model:"v3-beta",content:"how are you?"})
 * @type {string} The Question You Want to Ask Artificial Intelligence.
 * @returns {Hercai}
 * @async
 */
-async question({model = "v3",content}){
+async question({model = "v3",content,personality=""}){
 if(!["v3","gemini","v3-32k","turbo","turbo-16k"].some(ind => model == ind)) model = "v3";
 if(!content || content == undefined || content == null)throw new Error("Please specify a question!");
 try{
 var api = await axios.get(`https://hercai.onrender.com/${model}/hercai?question=`+encodeURI(content),{
     headers: {
         "content-type": "application/json",
+        "Authorization": this.apiKey,
     },
     data:{
-    key:this.apiKey
-    },
+        personality: personality
+    }
 })
 return api.data;
 }catch(err){
@@ -60,17 +62,14 @@ throw new Error("Error: "+ err.message)
 * @returns {Hercai}
 * @async
 */
-async drawImage({model = "v3",prompt,negative_prompt}){
+async drawImage({model = "v3",prompt,negative_prompt=""}){
     if(!["v1","v2","v2-beta","v3","lexica","prodia","simurg","animefy","raava","shonin"].some(ind => model == ind)) model = "prodia";
     if(!prompt || prompt == undefined || prompt == null)throw new Error("Please specify a prompt!");
-    if(!negative_prompt || negative_prompt == undefined || negative_prompt == null) negative_prompt = "";
     try{
     var api = await axios.get(`https://hercai.onrender.com/${model}/text2image`+"?prompt="+encodeURI(prompt)+"&negative_prompt="+encodeURI(negative_prompt),{
         headers: {
             "content-type": "application/json",
-        },
-        data:{
-        key:this.apiKey
+            "Authorization": this.apiKey,
         },
     })
     return api.data;
